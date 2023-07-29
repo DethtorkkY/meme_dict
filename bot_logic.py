e670ac18b05c07a4ec6f4a9de1885f38ed2fb772
DethtorkkY <138982852+DethtorkkY@users.noreply.github.com>
2023-07-29 18:20:09 +0600

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,23 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["(UwU)", ":)", ":(", ":3", "xD",":O"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
